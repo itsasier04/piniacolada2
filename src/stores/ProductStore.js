2d1e9fd272e53d9b24c175d25640b2e60a54014a
Asier <asier.barranco.7e6@itb.cat>
2024-01-11 18:25:05 +0100

    Fin del projecte

--- src/stores/ProductStore.js
+++ src/stores/ProductStore.js
@@ -1,12 +1,10 @@
 import { defineStore } from "pinia";
 
-//nom i option object
 export const useProductStore = defineStore("ProductStore", {
-  //state
+
   state: () => {
     return {
       products: [],
-      //es lo mismo que "products: products"
     };
   },
   actions: {
